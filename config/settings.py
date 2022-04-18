219c0e1edcbd07b6c1657fa13351e448292ccbe7
hyejin <doghj0105@daum.net>
2022-04-18 15:16:28 +0900

    시크릿키, 데이터베이스 정보 보안

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+import my_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,25 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-import json
-from django.core.exceptions import ImproperlyConfigured
-
-
-secret_file = os.path.join(BASE_DIR, 'secrets.json')
-
-with open(secret_file) as f:
-    secrets = json.loads(f.read())
-
-def get_secret(setting, secrets=secrets):
-
-    try:
-        return secrets[setting]
-    except KeyError:
-        error_msg = "Set the {} environment variable".format(setting)
-        raise ImproperlyConfigured(error_msg)
-
-SECRET_KEY = get_secret("SECRET_KEY")
-
+SECRET_KEY = my_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -64,7 +47,6 @@ INSTALLED_APPS = [
     "qandaBoard",
     "pricePredict",
     "chatbot",
-    "bbuser",
 ]
 
 MIDDLEWARE = [
@@ -102,18 +84,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        'NAME': 'BobBit',
-        'USER': 'admin',
-        'PASSWORD': 'bobbit123',
-        'HOST': 'bobbit.cpf4iis0vkig.us-east-1.rds.amazonaws.com',
-        'PORT': 3306,
-        'option': "SET sql_mode='STRICT_TRANS_TABLES'"
-    }
-}
-
+DATABASES = my_settings.DATABASES
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
