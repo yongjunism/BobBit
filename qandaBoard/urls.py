75a592d822ff10263fce3bc69847e556f5f18b90
hyejin <doghj0105@daum.net>
2022-04-19 10:39:02 +0900

    fixing .gitignore

---
+++ qandaBoard/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+app_name = 'qandaBoard'
+urlpatterns = [
+        path('qna', views.index, name='index'),
+        path('post', views.post, name='post')
+] 
--- qandaBoard/views.py
+++ qandaBoard/views.py
@@ -1,3 +1,30 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from django.core.paginator import Paginator
+from .models import *
+
 
 # Create your views here.
+def index(request):
+    #페이징
+    page = request.GET.get('page', '1')  # 페이지
+    #boards = {'boards': Board.objects.all().order_by('-board_reg_date')}
+    boards = Board.objects.all().order_by('-board_reg_date')
+    # 페이징처리
+    paginator = Paginator(boards, 7)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    context = {'boards': page_obj}
+    return render(request, 'qandaBoard/list.html', context)
+    
+
+def post(request):
+    if request.method == "POST":
+        author = request.POST['author']
+        title = request.POST['title']
+        content = request.POST['content']
+        board = Board(author=author, title=title, content=content)
+        board.save()
+        return HttpResponseRedirect(reverse('index'))
+    else:
+        return render(request, 'qandaBoard/post.html')
