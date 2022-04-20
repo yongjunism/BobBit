90863019ff968a4db69ab4b89719be7a43c3ce76
hyejin <doghj0105@daum.net>
2022-04-20 11:06:08 +0900

    게시판 기본적인 기능

--- qandaBoard/urls.py
+++ qandaBoard/urls.py
@@ -7,5 +7,6 @@ from django.conf.urls.static import static
 app_name = 'qandaBoard'
 urlpatterns = [
         path('qna', views.index, name='index'),
-        path('post', views.post, name='post')
+        path('post', views.post, name='post'),
+        path('post/<int:post_id>/', views.detail, name='detail'),
 ] 
--- qandaBoard/views.py
+++ qandaBoard/views.py
@@ -1,30 +1,47 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
 from django.core.paginator import Paginator
 from .models import *
+from django.db import transaction
+from datetime import datetime
+from django.shortcuts import render, redirect
 
-
-# Create your views here.
+#게시판 리스트
 def index(request):
     #페이징
     page = request.GET.get('page', '1')  # 페이지
     #boards = {'boards': Board.objects.all().order_by('-board_reg_date')}
-    boards = Board.objects.all().order_by('-board_reg_date')
+    boards = Board.objects.filter(board_deleted='N').order_by('-board_reg_date')
     # 페이징처리
-    paginator = Paginator(boards, 7)  # 페이지당 10개씩 보여주기
+    paginator = Paginator(boards, 7)  # 페이지당 7개씩 보여주기
     page_obj = paginator.get_page(page)
     context = {'boards': page_obj}
     return render(request, 'qandaBoard/list.html', context)
-    
 
+#질문 남기기   
+# @transaction.atomic
 def post(request):
-    if request.method == "POST":
-        author = request.POST['author']
-        title = request.POST['title']
-        content = request.POST['content']
-        board = Board(author=author, title=title, content=content)
-        board.save()
-        return HttpResponseRedirect(reverse('index'))
-    else:
-        return render(request, 'qandaBoard/post.html')
+    try:
+        if request.method == "POST":
+            board_title = request.POST['board_title']
+            board_content = request.POST['board_content']
+            # user_id=request.session.get('user_id')
+            # user = User.objects.get(pk=user_id)
+            board = Board(
+                board_title=board_title, 
+                board_content=board_content, board_reg_date = datetime.now() )
+            board.save()
+            return redirect('/qna')
+        else:
+            return render(request, 'qandaBoard/post.html')
+    except:
+        return HttpResponseRedirect(content='404 NOT FOUND')
+
+#질문 상세 조회
+from django.http import HttpResponseRedirect, Http404
+def detail(request, post_id):
+    try:
+        board = Board.objects.get(pk=post_id)
+    except Board.DoesNotExist:
+        raise Http404("Does not exist!")
+    return render(request, 'qandaBoard/detail.html', {'board': board})
