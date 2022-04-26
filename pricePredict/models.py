f5917e292aa82628f45c4d8d245d8a4c042f150f
최승훈 <chhoon97@naver.com>
2022-04-26 14:45:15 +0900

    0426 model change

--- pricePredict/models.py
+++ pricePredict/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models.fields import CharField, IntegerField, FloatField, DateField
+from django.conf import settings
 # Create your models here.
 
 
@@ -22,6 +23,17 @@ class Product(models.Model):
     pImage = CharField(max_length=255, null=True)
     cKey = models.ForeignKey(
         Categori, on_delete=models.CASCADE, db_column='cKey', null=True)
+
+    # user = models.ForeignKey(settings.AUTH_USER_MODEL,
+    #                          on_delete=models.CASCADE)
+    wish_user = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        blank=True,
+        related_name='wish_user'
+    )
+
+    def count_wish_user(self):
+        return self.pWishcount.count()
 
     class Meta:
         db_table = 'Product'
