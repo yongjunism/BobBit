1b9b0651b2c86b47fd97716379925e7a0f69be72
최승훈 <chhoon97@naver.com>
2022-04-28 16:34:46 +0900

    0428 품목분석완성

--- pricePredict/views.py
+++ pricePredict/views.py
@@ -1,7 +1,7 @@
 from operator import mod
 from django.shortcuts import render
 from sympy import product
-from .models import Product, Categori
+from .models import Product, Categori, VirtualProduct
 from django.forms.models import model_to_dict
 from datetime import date
 from .categori_to_emoji import EMOJI
@@ -9,18 +9,22 @@ import pandas as pd
 import joblib
 import requests
 from bs4 import BeautifulSoup
+from django.contrib.auth import get_user_model
 
 # Create your views here.
 import json
-from django.http import HttpResponse
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 
+
 def CrawlingCoupang(product_name):
     # 쿠팡 크롤링하는 부분
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
     url = f'https://www.coupang.com/np/search?component=&q=' + product_name
-    response = requests.get(url)
+    response = requests.get(url, headers=headers)
     response.encoding = 'utf-8'
     bs = BeautifulSoup(response.text, "html.parser")
     elements = bs.select("#productList > .search-product")[:4]
@@ -48,6 +52,14 @@ def priceViewbyParam(request, product_id):
     lastmonth = today.month
 
     product = Product.objects.get(pNo=product_id)
+    #좋아요 되어잇는지 아닌지
+    isLike = None
+    user = request.user
+    if product.wish_user.filter(id=user.id).exists():
+        isLike = True
+    else:
+        isLike = False
+
     product_dict = model_to_dict(product)
     cNo = product_dict['cKey']
 
@@ -81,7 +93,6 @@ def priceViewbyParam(request, product_id):
     pd_list = []
     product_info = product_dict
 
-
     for item in categori_list:
         pd_list.append({
             'pName': model_to_dict(item)['pName'],
@@ -90,7 +101,8 @@ def priceViewbyParam(request, product_id):
         })
 
     # ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ크롤링 ㅡㅡㅡㅡㅡㅡㅡㅡㅡ
-    crawlingdata = CrawlingCoupang(product_info['pName'])
+    # crawlingdata = CrawlingCoupang(product_info['pName'])
+    crawlingdata = []
 
     if request.method == 'GET':
         return render(
@@ -103,25 +115,56 @@ def priceViewbyParam(request, product_id):
              'product_info': product_info,  # 해당 상품의 db 데이터
              'pd_data': pd_data,        # 해당 상품의 지금까지 csv데이터
              'next_price': next_price,  # 다음달 상품 가격
-             'now_price': now_price,
-             'wish_count': model_to_dict(item)['wish_user'].count_wish_user()}  # 현재 상품 가격
-        )
+             'now_price': now_price,    # 현재 상품 가격
+             'wish_count': product.count_wish_user(),
+             'isLike' : isLike,
+             })
+
 
 @login_required
 @require_POST
-
 def product_wish(request, product_id):
-    product = get_object_or_404(Product, pk=product_id)
-    user = request.user
-    message = None
+    if request.method == 'POST':
+        product = get_object_or_404(Product, pk=product_id)
+        user = request.user
+        message = None
 
-    
-    if product.wish_user.filter(id=user.id).exists():
-        product.wish_user.remove(user)
-        message = '좋아요 취소'
-    else:
-        product.wish_user.add(request.user)
-        message = '좋아요'
+        if product.wish_user.filter(id=user.id).exists():
+            product.wish_user.remove(user)
+            message = '좋아요 취소'
+        else:
+            product.wish_user.add(request.user)
+            message = '좋아요'
 
-    context = {'wish_count': product.count_wish_user(), 'message': message}
-    return HttpResponse(json.dumps(context), content_type="application/json")
+        context = {'wish_count': product.count_wish_user(), 'message': message}
+        return JsonResponse(context, safe=False)
+
+
+@login_required
+@require_POST
+def product_buy(request, product_id):
+    if request.method == 'POST':
+        try:
+            quantityVal = request.POST['quantityVal']
+            priceVal = request.POST['priceVal']
+            User = get_user_model()
+            user = get_object_or_404(User, username=request.user)
+            user.point = user.point - (int(priceVal) * int(quantityVal))
+            user.save()
+
+            if(get_object_or_404(VirtualProduct, pNo=product_id, userID=user) == None):
+                virtualProduct = VirtualProduct(
+                    pNo=get_object_or_404(Product, pNo=product_id),
+                    pNum=int(quantityVal),
+                    userID=user
+                )
+                virtualProduct.save()
+            else:
+                virtualProduct = get_object_or_404(
+                    VirtualProduct, pNo=product_id, userID=user)
+                virtualProduct.pNum = virtualProduct.pNum + int(quantityVal)
+                virtualProduct.save()
+
+            return JsonResponse({'content': True}, safe=False)
+        except Exception as e:
+            return JsonResponse({'content': str(e)}, safe=False)
