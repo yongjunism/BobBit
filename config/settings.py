30c79f82f2c455855374505a0b2f1c402b2a3e22
doublemano <tjdkgus2@naver.com>
2022-05-09 09:56:19 +0900

    sah

--- config/settings.py
+++ config/settings.py
@@ -14,6 +14,10 @@ import os
 from pathlib import Path
 import my_settings
 
+import pymysql  
+pymysql.install_as_MySQLdb()
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
