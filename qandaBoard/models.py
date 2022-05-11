80f72d8fd338bab9b13d43eb3904d76af346a340
hyejinkang99 <doghj0105@daum.net>
2022-05-11 11:30:19 +0900

    flake8 보완

--- qandaBoard/models.py
+++ qandaBoard/models.py
@@ -6,7 +6,7 @@ from bbuser.models import User
 class Board(models.Model):
     board_id = AutoField(primary_key=True, db_column='board_id')
     board_title = CharField(max_length=50, null = False, db_column='board_title')
-    board_content = TextField(max_length = None, null = False, db_column='board_content' )
+    board_content = TextField(max_length = None, null = False, db_column='board_content')
     board_reg_date = DateTimeField(auto_now_add=True, null = False, db_column='board_reg_date')
     board_mod_date = DateTimeField(auto_now=True, null = True, db_column='board_mod_date')
     board_deleted = CharField(max_length=1, default = 'N', db_column='board_deleted')
